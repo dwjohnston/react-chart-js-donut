b19df120fc1d38c35d151b763f4623868e949c15
David Johnston <djohnston@cydarm.com>
2022-02-22 15:43:53 +1100

    Everything

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,37 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Doughnut data={{
+          datasets:
+            [{
+              backgroundColor: ["#f00", "#0f0"],
+              data: [1, 2, 3]
+            }
+            ],
+          labels: ["foo", "bar"]
+        }}
+          options={{
+            elements: {
+
+              //@ts-ignore
+              center: {
+                text: "hello",
+                color: '#ffffff',
+                minFontSize: 20 // Default is 20 (in px), set to false and text will not wrap.
+              }
+            },
+
+          }}
+        />
       </header>
     </div>
   );
